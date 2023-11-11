892417ffc47fe7c8edbfe5a6f93b962ddcc10d34
Samira77-ss <samiraseyfii0709@gmail.com>
2023-11-11 10:52:33 +0330

    Added HTML and CSS structures to start building the page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,55 @@
 import "./App.css";
-
-function App() {
-  return <div className="App">hi</div>;
+import iconList from "./images/icon-list.svg";
+import illustrationDesktop from "./images/illustration-sign-up-desktop.svg";
+import illustrationMobile from "./images/illustration-sign-up-mobile.svg";
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row">
+          <div className="col-6 content">
+            <h2>Stay updated!</h2>
+            <p>Join 60,000+ product managers receiving monthly updates on:</p>
+            <ul>
+              <li>
+                <img src={iconList} className="icon" alt="iconList" />
+                <span>Product discovery and building what matters</span>
+              </li>
+              <li>
+                <img src={iconList} className="icon" alt="iconList" />
+                <span>Measuring to ensure updates are a success</span>
+              </li>
+              <li>
+                <img src={iconList} className="icon" alt="iconList" />
+                <span>And much more!</span>
+              </li>
+            </ul>
+            <form>
+              <div className="form">
+                <div className="email">
+                  <label for="email">Email address</label>
+                </div>
+                <input
+                  name="email"
+                  id="email"
+                  type="email"
+                  placeholder="email@company.com"
+                ></input>
+              </div>
+              <button type="submit" className="btn">
+                Subscribe to monthly newsletter
+              </button>
+            </form>
+          </div>
+          <div className="col-6">
+            <img
+              src={illustrationDesktop}
+              className="desktopImage"
+              alt="illustrationDesktop"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default App;
